4d865e459f1c5e1607e5938728bc28b57679e088
Anton Lisovski <lisovskijj.99@gmail.com>
2022-09-27 17:16:45 +0300

    t2

--- src/Task2/Main.java
+++ src/Task2/Main.java
@@ -1,13 +1,16 @@
-package Task2;
+package task2;
 
 public class Main {
     public static void main(String[] args) {
+        Doctor [] arrayDoctors = new Doctor[3];
+        arrayDoctors[0]=new Surgeon();
+        arrayDoctors[1]=new Dentist();
+        arrayDoctors[2]=new Therapist();
+        Patient patient=new Patient();
+       TreatmentPlan treatmentPlan=new TreatmentPlan();
 
-        TreatmentPlan plan=new TreatmentPlan();
+//
 
-        if (numberOfTreatmentPlan == 0) {
-            System.out.println(name.getName() + " " + dentist.treat());
-        }
 
     }
 }
--- src/Task2/TreatmentPlan.java
+++ src/Task2/TreatmentPlan.java
@@ -1,31 +1,14 @@
-package Task2;
+package task2;
 
-public class TreatmentPlan extends Patient {
-
-
-   public TreatmentPlan(int numberOfTreatmentPlan) {
-      this.numberOfTreatmentPlan = numberOfTreatmentPlan;
-   }
-
-   public int getNumberOfTreatmentPlan() {
-      return numberOfTreatmentPlan;
-   }
-
-   public void setNumberOfTreatmentPlan(int numberOfTreatmentPlan) {
-      this.numberOfTreatmentPlan = numberOfTreatmentPlan;
-   }
-    Patient p1 = new Patient("Anton",1);
-    Patient p2 = new Patient("Andrey",2);
-    Patient p3 = new Patient("Artem",3);
-
-    Doctor dentist = new Dentist();
-    Doctor surgeon = new Dentist();
-    Doctor therapist = new Dentist();
-
+public class TreatmentPlan  {
+   private int numberOfTreatmentPlan;
 
   public void addThreat(){
-     p1.getName()+p1.numberOfTreatmentPlan
+      this.numberOfTreatmentPlan=(int) (Math.random()*3);
   }
 
+    public int getNumberOfTreatmentPlan(){
+        return numberOfTreatmentPlan;
+    }
 
 }
